02af2f5d4f62d7feecab1c95ffcc879e69b3fa0a
gjimenez523 <gabe.jimenez@outlook.com>
2016-01-13 13:59:41 -0700

    Initial - Updated Video Sample 2

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var static = require('node-static');
 var http = require('http');
+var port = process.env.port || 2013;
 var file = new(static.Server)();
 var app = http.createServer(function (req, res) {
   file.serve(req, res);
-}).listen(2013);
+}).listen(port);
